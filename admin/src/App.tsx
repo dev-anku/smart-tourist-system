f67def5860023034e1e7fd2e382b880fea8d4cb3
Sumit Kumar <sumit.k.budaniya@gmail.com>
2025-09-09 00:19:18 +0530

    LOGIN

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/dashboard"
+import Login from "./pages/login"
 
 function App() {
 
 
   return (
      <Routes>
-      <Route path="/" element={<Dashboard />}/>
+      <Route path="/" element={<Login />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+
 
       
      </Routes>
