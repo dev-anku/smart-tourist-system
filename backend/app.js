d34d58f2f9a7da0967172e721086e8ff2c9d21dc
Anku <ankitkumarmuhal3@gmail.com>
2025-09-08 15:36:07 +0530

    Initialized backend

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGODB).catch((err) => {
+  console.error(err);
+});
+
+module.exports = app;
