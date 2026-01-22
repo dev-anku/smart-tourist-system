2241bd8c1ba65162e5c46e8fd17130e3a5ce0d58
Anku <ankitkumarmuhal3@gmail.com>
2026-01-22 22:14:04 +0530

    Uncommited changes -- deleting local repo

--- admin/src/components/list.tsx
+++ admin/src/components/list.tsx
@@ -1,21 +1,15 @@
-const List = () =>{
-    return(
-
-
-        <div className="w-[20%] bg-black h-full z-1 shadow-sm">
-
-            <ul className="gap-3 list-none flex flex-col p-3">
-                <li className="p-4 flex flex-col border cursor-pointer border-white/20 rounded-xl hover:bg-[#121212]">
-                    <p className="text-white/80 font-semibold text-lg">Sumit</p>
-                    <p className="text-white/50 text-sm">sumit.k.budnaniya@gmail.com</p>
-                    <p className="text-white/50 text-sm">8209333127</p>
- 
-                </li>
-            </ul>
-
-            
-        </div>
-    )
+const List = () => {
+  return (
+    <div className="w-[20%] bg-black h-full z-1 shadow-sm">
+      <ul className="gap-3 list-none flex flex-col p-3">
+        <li className="p-4 flex flex-col border cursor-pointer border-white/20 rounded-xl hover:bg-[#121212]">
+          <p className="text-white/80 font-semibold text-lg">Sumit</p>
+          <p className="text-white/50 text-sm">sumit.k.budnaniya@gmail.com</p>
+          <p className="text-white/50 text-sm">8209333127</p>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default List;
--- admin/src/pages/dashboard.tsx
+++ admin/src/pages/dashboard.tsx
@@ -19,25 +19,25 @@ const Dashboard = () => {
         <List />
       </div>
 
-    {selected === "settings" && (
-  <div className="fixed inset-0 bg-black/50 backdrop-blur-lg flex items-center justify-center z-50 overflow-hidden">
-<div className="w-[90vw] max-w-[1000px] h-[80vh] p-6 bg-black border rounded-2xl shadow-lg flex flex-col items-center relative">
+      {selected === "settings" && (
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-lg flex items-center justify-center z-50 overflow-hidden">
+          <div className="w-[90vw] max-w-[1000px] h-[80vh] p-6 bg-black border rounded-2xl shadow-lg flex flex-col items-center relative">
 
-      <h2 className="text-xl font-semibold mb-4 text-white/80">Settings</h2>
-    
-   { !addRestrictedArea &&  <button onClick={()=>setAddRestrictedArea(true)} className="rounded-lg bg-white w-full font-semibold text-sm px-2 py-2 cursor-pointer">Add map</button>}
+            <h2 className="text-xl font-semibold mb-4 text-white/80">Settings</h2>
 
-{addRestrictedArea ? (<><AddRestrictedArea/> <button className="bg-white mt-auto px-4 text-sm py-1 rounded-lg font-semibold cursor-pointer">Add</button></>) : ''}
+            {!addRestrictedArea && <button onClick={() => setAddRestrictedArea(true)} className="rounded-lg bg-white w-full font-semibold text-sm px-2 py-2 cursor-pointer">Add map</button>}
 
-      <button
-        onClick={() => {setSelected("all"); setAddRestrictedArea(false)}}
-        className="absolute top-3 right-3 text-sm text-white/80 x-3 py-1 rounded-lg cursor-pointer bg-transparent"
-      >
-        close
-      </button>
-    </div>
-  </div>
-)}
+            {addRestrictedArea ? (<><AddRestrictedArea /> <button className="bg-white mt-auto px-4 text-sm py-1 rounded-lg font-semibold cursor-pointer">Add</button></>) : ''}
+
+            <button
+              onClick={() => { setSelected("all"); setAddRestrictedArea(false) }}
+              className="absolute top-3 right-3 text-sm text-white/80 x-3 py-1 rounded-lg cursor-pointer bg-transparent"
+            >
+              close
+            </button>
+          </div>
+        </div>
+      )}
 
     </>
   );
